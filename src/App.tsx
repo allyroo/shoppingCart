df588f5b9a362efe7b77245548202a7c19d99248
Allyse Arucan <81602578+allyroo@users.noreply.github.com>
2022-04-01 03:50:54 -0700

    first

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,66 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { ChangeEvent, useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
+  const [searchResults, setSearchResults] = useState([]);
+  const [foodQuery, setFoodQuery] = useState("");
+  const [toBuy, setToBuy] = useState([foodQuery]);
+
+  useEffect(() => {
+    fetch(`https://api.frontendeval.com/fake/food/${foodQuery}`)
+      .then((response) => response.json())
+      .then((data) => setSearchResults(data));
+  }, [foodQuery]);
+
+  function addGrocery(event: React.MouseEvent<HTMLButtonElement>) {
+    event.preventDefault();
+    setToBuy([...toBuy]);
+  }
+
+  // function handleChange(event: React.MouseEvent<HTMLElement>) {
+  //   setToBuy(event.target.value);
+  // }
+
+  // function handleClick(event: React.MouseEvent<HTMLElement>) {
+  //   event.preventDefault();
+  // }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="title">
+        <h1> My Shopping List</h1>
+      </div>
+      <form>
+        <input
+          type="text"
+          value={foodQuery}
+          list="item"
+          onChange={(e) => setFoodQuery(e.target.value)}
+        />
+      </form>
+
+      <div className="shopping-list">
+        {/* <form onSubmit={addGrocery}> */}
+        <datalist id="item">
+          {searchResults.map((grocery, index) => (
+            <select>
+              {/* // <select onClick={addGrocery}> */}
+              <option key={index}>{grocery}</option>
+            </select>
+          ))}
+        </datalist>
+        <button type="submit">Add Item</button>
+        {/* </form> */}
+      </div>
+      <div className="to-buy-list">
+        {toBuy.map((index) => (
+          <ul>
+            <div>
+              <li key={index}>{foodQuery}</li>
+            </div>
+          </ul>
+        ))}
+      </div>
     </div>
   );
 }
