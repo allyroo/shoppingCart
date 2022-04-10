484f1e78bd1ff8d12aec82ceae26ee5250c860cc
Allyse Arucan <81602578+allyroo@users.noreply.github.com>
2022-04-10 00:06:40 -0700

    Complete v1 of shopping list

--- src/App.tsx
+++ src/App.tsx
@@ -1,65 +1,103 @@
-import React, { ChangeEvent, useEffect, useState } from "react";
+import React, { ChangeEvent, MouseEvent, useEffect, useState } from "react";
+import { IGrocery } from "./Interfaces";
 import "./App.css";
 
 function App() {
-  const [searchResults, setSearchResults] = useState([]);
-  const [foodQuery, setFoodQuery] = useState("");
-  const [toBuy, setToBuy] = useState([foodQuery]);
+  const [searchResults, setSearchResults] = useState<Array<string>>([]);
+  const [foodQuery, setFoodQuery] = useState<string>("");
+  const [toBuyList, setToBuyList] = useState<IGrocery[]>([]);
 
   useEffect(() => {
-    fetch(`https://api.frontendeval.com/fake/food/${foodQuery}`)
-      .then((response) => response.json())
-      .then((data) => setSearchResults(data));
+    if (foodQuery) {
+      fetch(`https://api.frontendeval.com/fake/food/${foodQuery}`)
+        .then((response) => response.json())
+        .then((data) => setSearchResults(data));
+    }
   }, [foodQuery]);
 
-  function addGrocery(event: React.MouseEvent<HTMLButtonElement>) {
+  function addGrocery(event: MouseEvent<HTMLButtonElement>): void {
     event.preventDefault();
-    setToBuy([...toBuy]);
+    const addItem = { foodQuery: foodQuery, isFound: false };
+    setToBuyList([...toBuyList, addItem]);
+    setFoodQuery("");
   }
 
-  // function handleChange(event: React.MouseEvent<HTMLElement>) {
-  //   setToBuy(event.target.value);
-  // }
+  function handleChange(event: ChangeEvent<HTMLInputElement>) {
+    setFoodQuery(event.target.value);
+  }
 
-  // function handleClick(event: React.MouseEvent<HTMLElement>) {
-  //   event.preventDefault();
-  // }
+  function deleteGrocery(deleteGroceryItem: string) {
+    setToBuyList(
+      toBuyList.filter((item) => item.foodQuery !== deleteGroceryItem)
+    );
+  }
+
+  function handleToggle(index: number) {
+    const updatedToBuyList = [...toBuyList];
+    updatedToBuyList[index].isFound = !updatedToBuyList[index].isFound;
+    setToBuyList(updatedToBuyList);
+  }
 
   return (
     <div className="container">
       <div className="title">
         <h1> My Shopping List</h1>
       </div>
-      <form>
+
+      <form className="search">
         <input
           type="text"
           value={foodQuery}
           list="item"
-          onChange={(e) => setFoodQuery(e.target.value)}
+          onChange={handleChange}
         />
+        <button className="addGroceryButton" onClick={addGrocery}>
+          Add Item
+        </button>
       </form>
 
       <div className="shopping-list">
-        {/* <form onSubmit={addGrocery}> */}
         <datalist id="item">
-          {searchResults.map((grocery, index) => (
-            <select>
-              {/* // <select onClick={addGrocery}> */}
-              <option key={index}>{grocery}</option>
+          {searchResults.map((grocery: string, index: number) => (
+            <select key={index}>
+              <option>{grocery}</option>
             </select>
           ))}
         </datalist>
-        <button type="submit">Add Item</button>
-        {/* </form> */}
       </div>
+
       <div className="to-buy-list">
-        {toBuy.map((index) => (
-          <ul>
-            <div>
-              <li key={index}>{foodQuery}</li>
-            </div>
-          </ul>
-        ))}
+        <ul className="list-items">
+          {toBuyList.map((grocery: IGrocery, index: number) => {
+            return (
+              <div className="grocery-list" key={index}>
+                <div className="grocery-item">
+                  <input
+                    type="checkbox"
+                    onClick={() => handleToggle(index)}
+                    className="checkbox"
+                    id="itemFound"
+                  />
+
+                  <li
+                    id="itemFound"
+                    className={grocery.isFound ? "item-found" : "item-to-seek"}
+                    key={index}
+                  >
+                    {grocery.foodQuery}
+                  </li>
+                </div>
+
+                <button
+                  className="deleteButton"
+                  onClick={() => deleteGrocery(grocery.foodQuery)}
+                >
+                  x
+                </button>
+              </div>
+            );
+          })}
+        </ul>
       </div>
     </div>
   );
---
+++ src/Interfaces.ts
@@ -0,0 +1,4 @@
+export interface IGrocery {
+  foodQuery: string;
+  isFound: boolean;
+}
